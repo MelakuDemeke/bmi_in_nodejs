6310ac46948b6c343cd4ac6a85c9d0a008e25e32
MelakuDemeke <melaku.demeke789@gmail.com>
2022-07-26 12:37:21 +0300

    full bmi code added

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.get("/", function(req, res){
     res.sendFile(__dirname+"/bmiCalculator.html");
 });
 
-
+app.post("/",function(req,res){
+    var h = Number(req.body.h);
+    var w = Number(req.body.w);
+    var hsq = h*h;
+    var bmi = w/hsq;
+    res.send("Your BMI is " +bmi);
+});
 
 app.listen(3000,function(){
     console.log("server started at port 3000");
